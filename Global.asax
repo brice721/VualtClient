48d174c8ca3cfcc990ed710d04adbc1827996743
brandon rice <brice721@hotmail.com>
2017-05-25 23:26:02 -0700

    Initial Commit For Vultr Deploy

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IpVault.MvcApplication" Language="C#" %>
---
+++ Scripts/angular-app/MainController.js
@@ -0,0 +1,8 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module('app', []);
+    app.controller('MainController', ['$scope', function ($scope) {
+        $scope.title = 'This Better Fucking Work!!!';
+    }]);
+})();
